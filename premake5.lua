0700190bf437e8e4a109b6089139f33fc7db6d11
KingoBoiii <kingodk@outlook.dk>
2026-02-01 19:19:55 +0100

    Add project files.

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+workspace "HelloABI"
+    configurations { "Debug", "Release" }
+
+-- Static library project
+project "HelloABI.StaticLib"
+    kind "StaticLib"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}/static"
+    objdir "bin-int/%{cfg.buildcfg}/static"
+
+    files { "src/**.h", "src/**.cpp" }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+
+-- Dynamic library project
+project "HelloABI.DynamicLib"
+    kind "SharedLib"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}/dynamic"
+    objdir "bin-int/%{cfg.buildcfg}/dynamic"
+
+    files { "src/**.h", "src/**.cpp" }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
